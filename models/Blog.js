e29114b7e1ada39d0b171d3b5cf9c668f88b5cd3
alesikivan <obcofficiall@gmail.com>
2022-10-07 16:53:35 +0300

    refactor (app): added new data to images

--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const Blog = new Schema({
   title: {type: String, require: true},
   content: {type: String, require: true},
-  image: {type: String, require: true},
+  image: {type: String},
   author: {type: Schema.Types.ObjectId, ref: 'User'},
   dateCreate: {type: Date, default: new Date()},
   dateUpdate: {type: Date, default: new Date()},
