c506e992435c3e947e9c0fc785b1c737e45aa381
alesikivan <obcofficiall@gmail.com>
2024-07-31 20:40:58 +0300

    changed ecosystem host and folder name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,10 +10,10 @@ module.exports = {
   deploy : {
     production : {
       user : 'root',
-      host : ['45.93.136.98'],
+      host : ['77.37.49.238'],
       ref  : 'origin/main',
       repo : 'https://github.com/alesikivan/api.ucomp.net',
-      path : '/root/apps/ucomp-api',
+      path : '/root/apps/api.ucomp.net',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 delete ucomp.server && pm2 start ucomp.server.js --name ucomp.server && pm2 save --force',
       'pre-setup': ''
